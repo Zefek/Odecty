8b21d3b286c80fb5a6312c252adf7d8b7ca815df
Zefek <petr.sponky@gmail.com>
2025-09-01 22:35:59 +0200

    Refactor message queue routing key to constant
    Replaced hardcoded routing key string in GaugeService with a constant from the new MessageQueueRoutingKeys class. This improves maintainability and reduces the risk of typos in routing key usage.

---
+++ OdectyStat/Dto/MessageQueueRoutingKeys.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdectyStat1.Dto;
+public static class MessageQueueRoutingKeys
+{
+    public const string Odecty_Gauge_Lastvaluechanged = "odecty.gauge.lastvaluechanged";
+}
