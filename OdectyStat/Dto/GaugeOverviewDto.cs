31601de7954e82f312128195bac64a24b2918ebe
Zefek <petr.sponky@gmail.com>
2026-05-19 20:10:29 +0200

    WebApi

---
+++ OdectyStat/Dto/GaugeOverviewDto.cs
@@ -0,0 +1,11 @@
+namespace OdectyStat1.Dto;
+
+public class GaugeOverviewDto
+{
+    public int Id { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public decimal LastValue { get; set; }
+    public DateTime? LastMeasurementAt { get; set; }
+    public bool HasPhoto { get; set; }
+}
