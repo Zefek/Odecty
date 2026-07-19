df43599cd6cb7689c5fe209c0965b7a8492b8bd3
Zefek <petr.sponky@gmail.com>
2026-07-19 10:28:02 +0200

    Rozšíření diagnostiky

---
+++ OdectyStat/Migrations/Diagnostics/20260719082005_AddFwVersionAndOtaFailCountToLSSensor.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OdectyStat1.Migrations.Diagnostics
+{
+    /// <inheritdoc />
+    public partial class AddFwVersionAndOtaFailCountToLSSensor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "fw_version",
+                table: "ls_sensor_diagnostics",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ota_fail_count",
+                table: "ls_sensor_diagnostics",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "fw_version",
+                table: "ls_sensor_diagnostics");
+
+            migrationBuilder.DropColumn(
+                name: "ota_fail_count",
+                table: "ls_sensor_diagnostics");
+        }
+    }
+}
